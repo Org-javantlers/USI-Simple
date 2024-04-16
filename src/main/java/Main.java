93b1447c28dc87bb55b4f185421e36a752534287
HelioSousa2000027 <80171958+HelioSousa2000027@users.noreply.github.com>
2024-04-16 14:59:54 +0100

    Update Main.java
    Atualização do ficheiro JAVA para https://www.inf.usi.ch/faculty/soule/teaching/2015-fall/cc/SimpleMain.java.txt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,13 @@
-//package org.example;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
+import java.io.*;
+import org.antlr.v4.runtime.*;
 
-
-public class Main {
-    public static void main(String[] args) {
-        // A string de entrada que você quer analisar
-        String expression = "10 + 2 * 3 -  1000";
-        //String test  = "thisisaTest";
-        // Criar um lexer que processa a entrada
-        MathLexer lexer = new MathLexer(CharStreams.fromString(expression));
-
-        // Criar um stream de tokens a partir do lexer
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-        // Criar um parser que consome o stream de tokens
-        MathParser parser = new MathParser(tokens);
-
-        // Especificar a regra da gramática para começar a análise (e.g., a regra 'expr' da gramática Math.g4)
-        ParseTree tree = parser.expr(); // 'expr' é o nome da regra inicial na gramática Math.g4
-
-        // Imprimir a árvore de análise em formato LISP (útil para depuração)
-        System.out.println(tree.toStringTree(parser));
-    }
+public class SimpleMain {
+  public static void main(final String[] args) throws IOException {
+    String printSource = null, printSymTab = null,
+      printIR = null, printAsm = null;
+    SimpleLexer lexer = new SimpleLexer(new ANTLRInputStream(args[0]));
+    SimpleParser parser = new SimpleParser(new CommonTokenStream(lexer));
+    String postfix = parser.program().s;
+    System.out.println(postfix);
+  }
 }
