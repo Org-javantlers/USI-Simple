8988a240e28fbfefcb077f1fea92c7c77987066b
Helio Sousa <2000027@estudante.uab.pt>
2024-04-16 16:54:05 +0100

    Corrigi todos os Warnings

--- src/main/java/SimpleMain.java
+++ src/main/java/SimpleMain.java
@@ -1,12 +1,8 @@
-import java.io.*;
 import org.antlr.v4.runtime.*;
 
 public class SimpleMain {
-  public static void main(final String[] args) throws IOException {
-    String printSource = null, printSymTab = null,
-      printIR = null, printAsm = null;
+  public static void main(final String[] args) {
     String string = "90-5+57+93";
-//    SimpleLexer lexer = new SimpleLexer(CharStreams.fromString((args[0])));
     SimpleLexer lexer = new SimpleLexer(CharStreams.fromString((string)));
     SimpleParser parser = new SimpleParser(new CommonTokenStream(lexer));
     String postfix = parser.program().s;
